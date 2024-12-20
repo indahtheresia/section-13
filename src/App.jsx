072ea4edc75cc3f7a174607edf956de8a7e3e576
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-12-20 22:17:05 +0700

    state scheduling & batching

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ function App() {
 
   function handleSetCount(newCount) {
     setChosenCount(newCount);
-  };
+    setChosenCount((prevChosenCount) => prevChosenCount + 1); // both state updates are scheduled and they're guaranteed to be executed in the order in which they are scheduled.
+    console.log(chosenCount); // won't work!
+  }; // React performs state batching, which simply means that multiple state updates that are triggered from the same function, for example, are batched together and will only lead to one component function execution.
 
   return (
     <>
